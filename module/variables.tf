2a0cd0c2f9a77995d9146584434f120e635e64ba
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-08-15 11:54:52 +0200

    Modify variable validation regex

--- module/variables.tf
+++ module/variables.tf
@@ -1,10 +1,10 @@
 variable "container_name" {
   type        = string
   description = "Name of the container"
-  #validation {
-  #  condition     = can(regex("^[a-z]((?!-)[a-z-]){1,61}[a-z]$", var.container_name))
-  #  error_message = "Invalid input for container name. Must be between 3 and 63 characters, start and end with a letter, and contain only lowercase letters and hyphens."
-  #}
+  validation {
+    condition     = can(regex("^[a-z]([a-z-]{1,61}[a-z])?$", var.container_name))
+    error_message = "Invalid input for container name. Must be between 3 and 63 characters, start and end with a letter, and contain only lowercase letters and hyphens."
+  }
 }
 
 variable "storage_account_name" {
